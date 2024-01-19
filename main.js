935dcbdfaed44bb442a22195fbff10524271a380
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-19 13:15:49 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const crypto = require('crypto');
+const elliptic = require('elliptic');
+const ec = new elliptic.ec('secp256k1');
+
+// Step 1: Generate a private key
+const privateKey = ec.genKeyPair();
+console.log(privateKey)
