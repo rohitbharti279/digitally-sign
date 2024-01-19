f5e690e6d393aece5001ce117357054af7ae719a
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-19 13:23:06 +0530

    sign the message

--- main.js
+++ main.js
@@ -4,4 +4,10 @@ const ec = new elliptic.ec('secp256k1');
 
 // Step 1: Generate a private key
 const privateKey = ec.genKeyPair();
-console.log(privateKey);
+
+// Step 2: Sign the message
+const message = 'Hello, World';
+const hash = crypto.createHash('sha256').update(message).digest('hex');
+console.log(hash)
+const signature = privateKey.sign(hash);
+
