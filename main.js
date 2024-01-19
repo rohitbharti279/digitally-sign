9bbe3c0a0e61436cdefed532e96b88f091143acf
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-19 13:27:09 +0530

    final submit

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ const crypto = require('crypto');
 const elliptic = require('elliptic');
 const ec = new elliptic.ec('secp256k1');
 
-// Step 1: Generate a private key
+//Generate a private key
 const privateKey = ec.genKeyPair();
 
-// Step 2: Sign the message
+//Sign the message
 const message = 'Hello, World';
 const hash = crypto.createHash('sha256').update(message).digest('hex');
-console.log(hash)
 const signature = privateKey.sign(hash);
+
+//Verify the signature using the public key
+const publicKey = privateKey.getPublic();
+const isValid = ec.verify(hash, signature, publicKey);
+
+console.log("Message:", message);
+console.log("Hash:", hash);
+console.log("Signature:", signature);
+console.log("Is Signature Valid?", isValid);
+
 
